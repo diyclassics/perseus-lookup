8b2479d2c7912468f9ad081e029855be1f52b686
Patrick J. Burns <patrick@diyclassics.org>
2014-08-19 14:54:46 -0400

    Added some usage examples to the docstring.

--- latfind.py
+++ latfind.py
@@ -1,6 +1,20 @@
 #! /usr/bin/env python
 """
-Script to look up Latin word using Perseus and return a list of available short definitions. Written by @diyclassics 8.15.14.
+Script to look up Latin word using Perseus and return a list of available short definitions. Can take multiple words as arguments. Written by @diyclassics 8.15.14.
+
+e.g.
+$ python latfind.py verbum
+$ verbum > verbum: a word 
+
+$ python latfind.py curas
+$ curas > cura: trouble, care, attention, pains, industry, diligence, exertion
+$ curas > curo: to care for, take pains with, be solicitous for, look to, attend to, regard
+
+$python latfind.py verbum curas
+$ verbum > verbum: a word 
+$
+$ curas > cura: trouble, care, attention, pains, industry, diligence, exertion
+$ curas > curo: to care for, take pains with, be solicitous for, look to, attend to, 
 """
 
 # imports
@@ -63,3 +77,4 @@ if __name__ == '__main__':
     else:
         for item in sys.argv[1:]:
             main(item)
+            print "\n",
